5d99d11c786c69e72b0d2c12fe0b0ee150031b94
nxx5xxx <dnwnx123@naver.com>
2023-12-26 21:48:37 +0900

    vault backup: 2023-12-26 21:48:37

--- Excel/ExcelTest/src/excel/LoadExcel.java
+++ Excel/ExcelTest/src/excel/LoadExcel.java
@@ -1,10 +1,39 @@
 package excel;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class LoadExcel {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("파일명을 입력해주세요");
+    	String fileName = sc.nextLine()+".xlsx";
+    try (FileInputStream fileInputStream = new FileInputStream(fileName);
+            Workbook workbook = new XSSFWorkbook(fileInputStream)) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+           // 시트 선택 (첫 번째 시트를 선택합니다.)
+           Sheet sheet = workbook.getSheetAt(0);
 
-	}
+           // 모든 행 반복
+           for (Row row : sheet) {
+               // 행의 모든 셀 반복
+               for (Cell cell : row) {
+                   // 셀의 내용을 출력
+                   System.out.print(cell + "\t");
+               }
+               // 행이 끝날 때마다 개행
+               System.out.println();
+           }
 
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+   }
 }
