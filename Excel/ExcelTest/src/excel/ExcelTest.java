930b7a7d05989d20207cfc0e276e5414dae0d696
nxx5xxx <dnwnx123@naver.com>
2023-12-26 21:28:37 +0900

    vault backup: 2023-12-26 21:28:37

--- Excel/ExcelTest/src/excel/ExcelTest.java
+++ Excel/ExcelTest/src/excel/ExcelTest.java
@@ -1,104 +1,56 @@
 package excel;
-import java.awt.Color;
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
-import org.apache.poi.hssf.usermodel.HSSFCellStyle;
-import org.apache.poi.hssf.usermodel.HSSFFont;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFColor;
-import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class ExcelTest {
 
-	public static void main(String[] args) {
-		
-		// .xls 확장자 지원
-		
-		// HSSFWorkbook hssWb = null;
-		// HSSFSheet hssSheet = null;
-		// Row hssRow = null;
-		// Cell hssCell = null;
-		
-		//.xlsx 확장자 지원
-		
-		XSSFWorkbook xssfWb = null; 
-		XSSFSheet xssfSheet = null; 
-		XSSFRow xssfRow = null; 
-		XSSFCell xssfCell = null;
-			
-		try {
-			int rowNo = 0; // 행의 갯수 
-	
-			xssfWb = new XSSFWorkbook(); //XSSFWorkbook 객체 생성
-			xssfSheet = xssfWb.createSheet("워크 시트1"); // 워크시트 이름 설정
-			
-			// 폰트 스타일
-			XSSFFont font = xssfWb.createFont();
-			font.setFontName(HSSFFont.FONT_ARIAL); // 폰트 스타일
-			font.setFontHeightInPoints((short)20); // 폰트 크기
-			font.setBold(true); // Bold 설정
-			font.setColor(new XSSFColor(Color.decode("#457ba2"))); // 폰트 색 지정
-			
-			//테이블 셀 스타일
-			CellStyle cellStyle = xssfWb.createCellStyle();
-			xssfSheet.setColumnWidth(0, (xssfSheet.getColumnWidth(0))+(short)2048); // 0번째 컬럼 넓이 조절
-			
-			cellStyle.setFont(font); // cellStyle에 font를 적용
-			cellStyle.setAlignment(HSSFCellStyle.ALIGN_CENTER); // 정렬
-
-			//셀병합
-			xssfSheet.addMergedRegion(new CellRangeAddress(0, 1, 0, 4)); //첫행, 마지막행, 첫열, 마지막열 병합
-			
-			// 타이틀 생성
-			xssfRow = xssfSheet.createRow(rowNo++); // 행 객체 추가
-			xssfCell = xssfRow.createCell((short) 0); // 추가한 행에 셀 객체 추가
-			xssfCell.setCellStyle(cellStyle); // 셀에 스타일 지정
-			xssfCell.setCellValue("타이틀 입니다"); // 데이터 입력
-			
-			xssfSheet.createRow(rowNo++);
-			xssfRow = xssfSheet.createRow(rowNo++);  // 빈행 추가
-			
-			//테이블 스타일 설정
-			CellStyle tableCellStyle = xssfWb.createCellStyle();
-			tableCellStyle.setBorderTop((short) 5);    // 테두리 위쪽
-			tableCellStyle.setBorderBottom((short) 5); // 테두리 아래쪽
-			tableCellStyle.setBorderLeft((short) 5);   // 테두리 왼쪽
-			tableCellStyle.setBorderRight((short) 5);  // 테두리 오른쪽
-			
-			xssfRow = xssfSheet.createRow(rowNo++);
-			xssfCell = xssfRow.createCell((short) 0);
-			xssfCell.setCellStyle(tableCellStyle);
-			xssfCell.setCellValue("셀1");
-			xssfCell = xssfRow.createCell((short) 1);
-			xssfCell.setCellStyle(tableCellStyle);
-			xssfCell.setCellValue("셀2");
-			xssfCell = xssfRow.createCell((short) 2);
-			xssfCell.setCellStyle(tableCellStyle);
-			xssfCell.setCellValue("셀3");
-			xssfCell = xssfRow.createCell((short) 3);
-			xssfCell.setCellStyle(tableCellStyle);
-			xssfCell.setCellValue("셀4");
-			xssfCell = xssfRow.createCell((short) 4);
-			xssfCell.setCellStyle(tableCellStyle);
-			
-			String localFile = "/Users/hihi/" + "excelDownTest" + ".xlsx";
-			
-			File file = new File(localFile);
-			FileOutputStream fos = null;
-			fos = new FileOutputStream(file);
-			xssfWb.write(fos);
-	
-			if (fos != null) fos.close();
-		}catch(Exception e){
-	        	
-		}
+	public static void main(String[] args) throws IOException {
+		// Blank workbook
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        // Creating a blank Excel sheet
+        XSSFSheet sheet = workbook.createSheet("student Details");
+        // Creating an empty TreeMap of string and Object][] type
+        Map<String, Object[]> data = new TreeMap<String, Object[]>();
+        // Writing data to Object[]
+        // using put() method
+        data.put("1", new Object[] { "ID", "CITY", "STATE" });
+        data.put("2", new Object[] { 1, "Clanton", "Alabama" });
+        data.put("3", new Object[] { 2, "Cordova", "Alaska" });
+        data.put("4", new Object[] { 3, "Clifton", "Arizona" });
+        data.put("5", new Object[] { 4, "Arcadia", "California" });
+  
+	// Iterating over data and writing it to sheet
+	Set<String> keyset = data.keySet(); 
+        int rownum = 0;
+        for (String key : keyset) { 
+            // Creating a new row in the sheet
+            XSSFRow row = sheet.createRow(rownum++); 
+            Object[] objArr = data.get(key); 
+            int cellnum = 0; 
+            for (Object obj : objArr) { 
+                // This line creates a cell in the next column of that row
+                XSSFCell cell = row.createCell(cellnum++); 
+                if (obj instanceof String)
+                    cell.setCellValue((String)obj); 
+                else if (obj instanceof Integer)
+                    cell.setCellValue((Integer)obj);
+            }
+        }     
+        // Writing the workbook
+        FileOutputStream out = new FileOutputStream("gfgcontribute.xlsx");
+        workbook.write(out); 
+        // Closing file output connections
+        out.close();
 	}
 
-	}
+}
 
 
