267da514f8acffa71ec4f1496d16357470e3b87e
nxx5xxx <dnwnx123@naver.com>
2023-12-27 21:50:53 +0900

    update Excel자동화

--- Excel/ExcelTest/src/excel/LoadExcel.java
+++ Excel/ExcelTest/src/excel/LoadExcel.java
@@ -10,31 +10,43 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import org.apache.poi.ss.usermodel.CellType;
+
 public class LoadExcel {
-    public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
-    	System.out.println("파일명을 입력해주세요");
-    	String fileName = sc.nextLine()+".xlsx";
-    try (FileInputStream fileInputStream = new FileInputStream(fileName);
-            Workbook workbook = new XSSFWorkbook(fileInputStream)) {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("파일명을 입력해주세요");
+		String fileName = sc.nextLine() + ".xlsx";
+		try (FileInputStream fileInputStream = new FileInputStream(fileName);
+				Workbook workbook = new XSSFWorkbook(fileInputStream)) {
 
-           // 시트 선택 (첫 번째 시트를 선택합니다.)
-           Sheet sheet = workbook.getSheetAt(0);
-
-           // 모든 행 반복
-           for (Row row : sheet) {
-               // 행의 모든 셀 반복
-               for (Cell cell : row) {
-                   // 셀의 내용을 출력
-                   System.out.print(cell + "\t");
-               }
-               // 행이 끝날 때마다 개행
-               System.out.println();
-           }
-
-       } catch (IOException e) {
-           e.printStackTrace();
-       }
-    sc.close();
-   }
+			// 시트 선택 (첫 번째 시트를 선택합니다.)
+			Sheet sheet = workbook.getSheetAt(0);
+			// 모든 행 반복
+//           for (Row row : sheet) {
+//               // 행의 모든 셀 반복
+//               for (Cell cell : row) {
+//                   // 셀의 내용을 출력
+//                   System.out.print(cell + "\t");
+//               }
+//               // 행이 끝날 때마다 개행
+//               System.out.println();
+//           }
+			// sheet.getRow(1).getCell(1);
+			System.out.println("테스");
+			// row는 1부터시작 cell은 0부터시작
+			// 제목칼럼은 9부터
+			// row 의 개수를 알수있는메소드 getLastRowNum 해당 메소드는 0부터시작하므로 +1을해줘야함 
+			for(int i=0;i<sheet.getLastRowNum()+1;i++) {
+				//System.out.println(sheet.getRow(i).getCell(8));
+				if(sheet.getRow(i).getCell(8).getCellType() == CellType.NUMERIC) {
+				System.out.println((int)sheet.getRow(i).getCell(8).getNumericCellValue());
+				}
+			}
+		// 모든셀을 불러온후 미리 객체에 저장하는게 빠를거같은데 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		sc.close();
+	}
 }
---
+++ Excel/ExcelTest/src/excel/TwoExcelComparison.java
@@ -0,0 +1,65 @@
+package excel;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class TwoExcelComparison {
+
+	public static void main(String[] args) {
+		String fileName1 = "동성초.xlsx";
+		String fileName2 = "사본리스트.xlsx";
+		try(FileInputStream fileInputStream1 = new FileInputStream(fileName1);
+			FileInputStream fileInputStream2 = new FileInputStream(fileName2);
+			Workbook school = new XSSFWorkbook(fileInputStream1);
+			Workbook alllist = new XSSFWorkbook(fileInputStream2);	){
+			
+			Sheet ss = school.getSheetAt(0);
+			Sheet ls = alllist.getSheetAt(0);
+			HashMap<String, Integer> map = new HashMap<>();
+			List<Integer> list = new ArrayList<>();
+			int sw = 0;
+			for(int i=1;i<ss.getLastRowNum()+1;i++) {
+				//System.out.println(ss.getRow(i).getCell(1));
+				for(int j=2;j<ls.getLastRowNum()+1;j++) {
+					if(ls.getRow(j).getCell(9)==null)continue;
+					if(ls.getRow(j).getCell(9).toString().contains(ss.getRow(i).getCell(1).toString())) {
+						sw=1;
+						System.out.println(ss.getRow(i).getCell(1).toString());
+						System.out.println(ls.getRow(j).getCell(9).toString());
+//						System.out.println("--------------------------------");
+						if(ls.getRow(j).getCell(8).getCellType() == CellType.NUMERIC) {
+						list.add((int)ls.getRow(j).getCell(8).getNumericCellValue());
+						//System.out.println(ls.getRow(i).getCell(9));
+						}else {
+//							System.out.println(ls.getRow(i).getCell(8).getCellType());
+							System.out.println(ls.getRow(j).getCell(8));
+							list.add(00000);
+						}
+						break;
+					}else {
+						sw=0;
+						//list.add(00000);
+					}
+				}
+				if(sw==0) {
+					System.out.println(ss.getRow(i).getCell(1).toString());
+					System.out.println("-결과없음--------------------------");
+					list.add(00000);
+				}
+			}
+			System.out.println("test");
+			for(Integer data : list)System.out.println(data);
+			System.out.println("end test");
+		} catch (Exception e) {
+			System.out.println("error : "+e);
+		}
+	}
+
+}
