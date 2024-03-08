b31048970031540b277fbded642684d1c3a69a2b
nxx5xxx <126748625+nxx5xxx@users.noreply.github.com>
2024-03-08 17:17:43 +0900

    add savepoint
    체크박스하다가 일처리로인해 일시중단

--- Plan/nxxInserMemberProcedure.php
+++ Plan/nxxInserMemberProcedure.php
@@ -31,7 +31,9 @@ include_once("$g4_path/common.php");
 	$grade = $_POST["grade"];
 	$class = $_POST["class"];
 	$numb = $_POST["numb"];
-
+	$nxx_sw = $_POST["chk_btn"];
+	echo $nxx_sw[0]."체크박스1";
+	echo $nxx_sw[1]."체크박스2";
         foreach($nxx_dbList as $data){
 			echo "<span style='color:red;font-size:24px;'>".$data."</span><br>";
 			$nxx_sql = "select * from ".$data.".g4_member";
@@ -39,22 +41,23 @@ include_once("$g4_path/common.php");
 			//for($i = 0;$row=sql_fetch_array($nxx_result);$i++){}
 			echo "<hr>";
 			//앞에 0 추가하기위함
-			for($i = 1;$i<=$grade;$i++){
-				if(strlen($i)<2){
-					$i = "0".$i;
-				}
-				for($j = 1;$j<=$class;$j++){
-					if(strlen($j)<2){
-						$j = "0".$j;
-					}
-					for($l = 1;$l<=$numb;$l++){
-						if(strlen($l)<2){
-							$l = "0".$l;
-						}
-						echo "{$year}{$i}{$j}{$l}<br>";
-					}
-				}
-			}
+
+			// for($i = 1;$i<=$grade;$i++){
+			// 	if(strlen($i)<2){
+			// 		$i = "0".$i;
+			// 	}
+			// 	for($j = 1;$j<=$class;$j++){
+			// 		if(strlen($j)<2){
+			// 			$j = "0".$j;
+			// 		}
+			// 		for($l = 1;$l<=$numb;$l++){
+			// 			if(strlen($l)<2){
+			// 				$l = "0".$l;
+			// 			}
+			// 			echo "{$year}{$i}{$j}{$l}<br>";
+			// 		}
+			// 	}
+			// }
         }
 ?>
 
