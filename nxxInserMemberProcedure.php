d084f67e88a8444bb2e3a7090e07e7ebdb1a9772
nxx5xxx <126748625+nxx5xxx@users.noreply.github.com>
2024-03-11 11:55:20 +0900

    add js
    js

---
+++ nxxInserMemberProcedure.php
@@ -0,0 +1,88 @@
+<?
+// $sc_id = "mySQL 아이디입력";
+// $sc_pw = "비밀번호입력";
+// $sc_db = "aa574";
+
+// $mysql_host = "db IP입력";
+// $user_arr[] = $mysql_user = "$sc_id";
+// $password_arr[] = $mysql_password = "$sc_pw";
+// $db_arr[] = $mysql_db = "$sc_id";
+// 이것을 안해도 상관없음 
+
+
+$g4_path = ".."; // common.php 의 상대 경로
+include_once("$g4_path/common.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="euc-kr">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+<? if($_SERVER["REQUEST_METHOD"] == "POST"){
+	//텍스트에어리어 받아옴
+	$nxx_db = $_POST["nxx_db"];
+	//,기준으로 나눠서 배열생성
+	$nxx_dbList = explode(',',$nxx_db);
+
+	$year = $_POST["year"];
+	$grade = $_POST["grade"];
+	$class = $_POST["class"];
+	$numb = $_POST["numb"];
+
+	$goyu = $_POST["goyu"];
+	$numb2 = $_POST["numb2"];
+
+	$nxx_sw = $_POST["nxx_radio"];
+	echo $nxx_sw."라디오 테스트";
+        foreach($nxx_dbList as $data){
+			echo "<span style='color:red;font-size:24px;'>".$data."</span><br>";
+			$nxx_sql = "select * from ".$data.".g4_member";
+			$nxx_result = sql_query($nxx_sql);
+			//for($i = 0;$row=sql_fetch_array($nxx_result);$i++){}
+			echo "<hr>";
+			//앞에 0 추가하기위함
+			if($nxx_sw<=2){
+				echo "고유번호학년반번호<br>";
+					for($i = 1;$i<=$grade;$i++){
+						if(strlen($i)<2){
+							$i = "0".$i;
+						}
+						for($j = 1;$j<=$class;$j++){
+							if(strlen($j)<2){
+								$j = "0".$j;
+							}
+							for($l = 1;$l<=$numb;$l++){
+								if(strlen($l)<2){
+									$l = "0".$l;
+								}
+								echo "{$year}{$i}{$j}{$l}<br>";
+							}
+						}
+					}
+			}if($nxx_sw!=1 && $nxx_sw>=2){
+				echo "고유번호학생명수";
+					for($i = 1;$i<=$numb2;$i++){
+						if(strlen($i)<2){
+							$i = "00".$i;
+						}else if(strlen($i)<3){
+							$i = "0".$i;
+						}
+							echo "{$goyu}{$i}<br>";
+					}
+			}
+        }
+?>
+
+<hr>
+ <!-- 
+        자바의 split같은 함수
+        explode (문자열 분할기준 , 분할시킬 문자열변수명 , 옵션 없어도됨 분할개수 지정)
+        예시 : explod('-',$example)
+  -->
+
+<?} ?>
+</body>
+</html>
---
+++ nxxInsertMember.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="euc-kr">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<style>
+    #nxx_chk1:checked{
+
+    }
+</style>
+</head>
+<body>
+    변경점테스트
+    <?php $dateYear = substr(date("Y"),2); echo $dateYear; ?>
+    <form action="nxxInserMemberProcedure.php" method="post" name="nxx_form">
+        <textarea style="width:100%;text-align:left;min-height:600px" placeholder="해당하는 db명을 ,로 구분하기 예시: a288,a292" name="nxx_db" required></textarea>
+        <input type="radio" name="nxx_radio" id="nxx_chk1" value="1"> <span style="color:red">* 고유번호+학년+반+번호 (24010101 , a010101) 등</span><br>
+        <input type="radio" name="nxx_radio" id="nxx_chk2" value="3"> <span style="color:red">* 고유번호+학생 수 (a24001~a24500) 등 </span><br>
+        <input type="radio" name="nxx_radio" id="nxx_chk3" value="2" checked> <span style="color:red">* 두방식 다</span><br>
+        <div id="nxx_yn">
+        <input type="text" placeholder="고유번호(문자가능)" name="year" style="width:10%" value="<?php echo $dateYear; ?>" required/>
+        <input type="text" placeholder="총학년(숫자만)" name="grade" style="width:10%" required/>
+        <input type="text" placeholder="총반(숫자만)" name="class" style="width:10%" required/>
+        <input type="text" placeholder="총번호(숫자만)" name="numb" style="width:10%" required/>
+        <span style="color:red">* 예시 24 1 5 35 입력 시 24010101부터 24010535까지 만들어지도록함</span><br><br>
+        </div>
+        <div id="nxx_ygcn">
+        <input type="text" placeholder="고유번호(문자가능)" name="goyu" style="width:10%" value="<?php echo "a".$dateYear; ?>" required/>
+        <input type="text" placeholder="총번호(숫자만)" name="numb2" style="width:10%" required/>
+        <span style="color:red">* 예시 a24 500 입력 시 a24001부터 a24500까지 만들어지도록 함</span><br><br>
+        </div>
+        <input type="submit" value="전송" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="reset" value="리셋"/>
+    </form>
+    <script>
+        //온체인지 넣고 하기
+        var nxx_radios = document.querySelectorAll('input[type=radio][name="nxx_radio"]');
+        nxx_radios.forEach(radio => radio.addEventListener('change', () => {
+                alert(radio.value);
+                if(radio.value == 1){
+                    document.getElementsByName('goyu')[0].removeAttribute("required");
+                    document.getElementsByName('numb2')[0].removeAttribute("required");
+                }else if(radio.value == 3){
+                    document.getElementsByName('year')[0].removeAttribute("required");
+                    document.getElementsByName('grade')[0].removeAttribute("required");
+                    document.getElementsByName('class')[0].removeAttribute("required");
+                    document.getElementsByName('numb')[0].removeAttribute("required");
+                }
+            }
+        ));
+        // const radios = document.getElementsByName('nxx_radio');
+        //     for(var i=0;i<radios.length;i++){
+        //         if(radios[i].checked===true){ console.log(radios[i].value); } 
+        //         }
+    </script>
+</body>
+</html>
