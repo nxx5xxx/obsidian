79ab1cf8df586a81944e1071012b48c2c5a5d43b
nxx5xxx <126748625+nxx5xxx@users.noreply.github.com>
2024-03-11 15:39:03 +0900

    final
    학생일괄등록 프로시저 제작완료
    try catch부분이 먹지않아 sql_query (~,false) 로 처리했음
    그누보드부분때문인지 php버전이 낮아서그런건지 뭔지 정확히 파악이안됨

--- Plan/nxxInserMemberProcedure.php
+++ Plan/nxxInserMemberProcedure.php
@@ -36,14 +36,21 @@ include_once("$g4_path/common.php");
 	$numb2 = $_POST["numb2"];
 
 	$nxx_sw = $_POST["nxx_radio"];
-	echo $nxx_sw."라디오 테스트";
+	echo $nxx_sw."<h1>결과보고</h1>";
         foreach($nxx_dbList as $data){
 			echo "<span style='color:red;font-size:24px;'>".$data."</span><br>";
 			$nxx_sql = "select * from ".$data.".g4_member";
 			$nxx_result = sql_query($nxx_sql);
 			//for($i = 0;$row=sql_fetch_array($nxx_result);$i++){}
-			echo "<hr>";
+			
 			//앞에 0 추가하기위함
+			//rs.execute처럼 쿼리가 성공적으로 처리되면 +하기위함
+			//성공
+			$nxx_cnt1 = 0;
+			$nxx_cnt2 = 0;
+			//실패
+			$nxx_cnt_false1 = 0;
+			$nxx_cnt_false2 = 0;
 			if($nxx_sw<=2){
 				echo "고유번호학년반번호<br>";
 					for($i = 1;$i<=$grade;$i++){
@@ -58,21 +65,39 @@ include_once("$g4_path/common.php");
 								if(strlen($l)<2){
 									$l = "0".$l;
 								}
-								echo "{$year}{$i}{$j}{$l}<br>";
+								$nxx_gcn = sql_query("insert into {$data}.g4_member(mb_id,mb_name,mb_nick,mb_password,mb_datetime,mb_level,mb_1) values('{$year}{$i}{$j}{$l}','{$year}{$i}{$j}{$l}','{$year}{$i}{$j}{$l}','19225735263cd3c5',now(),3,1)",false);
+								if($nxx_gcn){
+									$nxx_cnt1++;
+								}else{
+									echo "{$year}{$i}{$j}{$l} 오류발생<br>";
+									$nxx_cnt_false1++;
+								}
 							}
 						}
 					}
-			}if($nxx_sw!=1 && $nxx_sw>=2){
-				echo "고유번호학생명수";
+				echo "학년반번호<br> 성공 : {$nxx_cnt1}<br> 실패 : {$nxx_cnt_false1}<br>";	
+			}
+			if($nxx_sw>=2){
+				echo "고유번호학생명수<br>";
 					for($i = 1;$i<=$numb2;$i++){
 						if(strlen($i)<2){
 							$i = "00".$i;
 						}else if(strlen($i)<3){
 							$i = "0".$i;
 						}
-							echo "{$goyu}{$i}<br>";
+							$nxx_gn = sql_query("insert into {$data}.g4_member(mb_id,mb_name,mb_nick,mb_password,mb_datetime,mb_level,mb_1) values('{$goyu}{$i}','{$goyu}{$i}','{$goyu}{$i}','19225735263cd3c5',now(),3,1)",false);
+							if($nxx_gn){
+								$nxx_cnt2++;
+							}else{
+								echo "{$goyu}{$i} 오류발생<br>";
+								$nxx_cnt_false2++;
+							}
 					}
+				echo "고유+학생수<br> 성공 : {$nxx_cnt2}<br> 실패 : {$nxx_cnt_false2}<br>";
 			}
+			
+			
+			echo "<hr>";
         }
 ?>
 
